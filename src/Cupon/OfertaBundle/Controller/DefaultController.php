afc2106c33872e9596269a4b37cf3314ecf82309
SaMa <sama@gbif.es>
2016-08-11 23:22:56 +0200

    Refactorizando el controlador: Gestión de errores, redirecciones y parámetros de configuración

--- src/Cupon/OfertaBundle/Controller/DefaultController.php
+++ src/Cupon/OfertaBundle/Controller/DefaultController.php
@@ -4,6 +4,8 @@ namespace Cupon\OfertaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DefaultController extends Controller
 {
@@ -11,14 +13,30 @@ class DefaultController extends Controller
     /**
      *
      */
-    public function portadaAction()
+    public function portadaAction($ciudad)
     {
+        /*if (null == $ciudad)
+        {
+            $ciudad = $this->container
+                            ->getParameter('cupon.ciudad_por_defecto');
+            return new RedirectResponse(
+                $this->generateUrl('portada', array('ciudad' => $ciudad))
+            );
+        }*/
+
         $em = $this->getDoctrine()->getManager();
 
         $oferta = $em->getRepository('OfertaBundle:Oferta')->findOneBy(array(
-            'ciudad'            => 8,
+            'ciudad'            => $ciudad)
             //'fechaPublicacion' => new \DateTime('today')
-        ));
+        );
+
+        if (!$oferta)
+        {
+            throw $this->createNotFoundException(
+                'No se ha encontrado la oferta del día en la ciudad seleccionada'
+            );
+        }
 
         return $this->render(
             'OfertaBundle:Default:portada.html.twig',
