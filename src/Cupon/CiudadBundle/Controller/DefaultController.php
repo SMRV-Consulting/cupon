72fc7198c32ac75359363c4baf508dc9d1bee2db
SaMa <sama@gbif.es>
2016-08-13 14:29:30 +0200

    Creando el redireccionamiento para cambiar la ciudad activa en cada momento según quiera el usuario

--- src/Cupon/CiudadBundle/Controller/DefaultController.php
+++ src/Cupon/CiudadBundle/Controller/DefaultController.php
@@ -3,11 +3,11 @@
 namespace Cupon\CiudadBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('CiudadBundle:Default:index.html.twig', array('name' => $name));
+    public function cambiarAction($ciudad){
+        return new RedirectResponse($this->generateUrl('portada', array('ciudad' => $ciudad)));
     }
 }
